d33de3d8e278c68f108f5906f1331b4fd6e5f1ca
jjwalsh87 <44454856+jjwalsh87@users.noreply.github.com>
2019-07-31 16:29:44 -0400

    nothing

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,10 +4,15 @@ import Banner from '../banner/Banner';
 class Home extends Component {
 	render() {
 		return (
+			<div> 
 			<div className='col-13 mx-auto '>
 				<Banner />
+				
 			</div>
-		);
+			<div>
+			</div>
+
+</div>		);
 	}
 }
 export default Home;
